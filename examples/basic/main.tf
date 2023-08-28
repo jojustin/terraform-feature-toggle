7eebac7038cbd23de014e1c186b04103873324a5
Josephine Eskaline Joyce <jojustin@in.ibm.com>
2023-08-28 10:22:13 +0530

    fix: samples
    sample addition

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -23,41 +23,41 @@ locals {
 }
 
 
-# data ibm_resource_group "resource_group" {
-#     name = var.resource_group_id
-# }
+data ibm_resource_group "resource_group" {
+    name = var.resource_group_id
+}
 
-# resource "ibm_is_vpc" "vpc1" {
-#   name = "vpc"
-#   resource_group = data.ibm_resource_group.resource_group.id
-# }
+resource "ibm_is_vpc" "vpc1" {
+  name = "vpc"
+  resource_group = data.ibm_resource_group.resource_group.id
+}
 
-# resource "ibm_is_subnet" "subnet1" {
-#   name                     = "${local.prefix}-subnet"
-#   vpc                      = ibm_is_vpc.vpc1.id
-#   zone                     = "${var.region}-1"
-#   total_ipv4_address_count = 256
-#   resource_group = data.ibm_resource_group.resource_group.id
-# }
+resource "ibm_is_subnet" "subnet1" {
+  name                     = "${local.prefix}-subnet"
+  vpc                      = ibm_is_vpc.vpc1.id
+  zone                     = "${var.region}-1"
+  total_ipv4_address_count = 256
+  resource_group = data.ibm_resource_group.resource_group.id
+}
 
 
-# resource "ibm_container_vpc_cluster" "cluster" {
-#   name              = "${local.prefix}-vpccluster"
-#   vpc_id            = ibm_is_vpc.vpc1.id
-#   flavor            = local.vpc_cluster_flavor
-#   worker_count      = local.worker_count
-#   resource_group_id = data.ibm_resource_group.resource_group.id
-#   zones {
-#     subnet_id = ibm_is_subnet.subnet1.id
-#     name      = "${var.region}-1"
-#   }
+resource "ibm_container_vpc_cluster" "cluster" {
+  name              = "${local.prefix}-vpccluster"
+  vpc_id            = ibm_is_vpc.vpc1.id
+  flavor            = local.vpc_cluster_flavor
+  worker_count      = local.worker_count
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  zones {
+    subnet_id = ibm_is_subnet.subnet1.id
+    name      = "${var.region}-1"
+  }
 
-# dynamic "kms_config" {
-#     for_each = local.kms_config != null && local.kms_config != {} ? [1] : []
-#     content {
-#       crk_id           = local.kms_config.crk_id
-#       instance_id      = local.kms_config.instance_id
-#       private_endpoint = local.kms_config.private_endpoint == null ? true : local.kms_config.private_endpoint
-#     }
-#   }
-# }
+dynamic "kms_config" {
+    for_each = local.kms_config != null && local.kms_config != {} ? [1] : []
+    content {
+      crk_id           = local.kms_config.crk_id
+      instance_id      = local.kms_config.instance_id
+      private_endpoint = local.kms_config.private_endpoint == null ? true : local.kms_config.private_endpoint
+    }
+  }
+}
